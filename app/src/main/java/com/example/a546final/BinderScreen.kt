357ec3da895325571fdda518015dc3a5191f16c6
iankruger <ian.kruger.2@gmail.com>
2025-05-02 23:51:57 -0400

    Added a search/filter feature for the card loading

--- app/src/main/java/com/example/a546final/BinderScreen.kt
+++ app/src/main/java/com/example/a546final/BinderScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,11 +18,15 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -51,6 +56,12 @@ fun BinderScreen(
     val photos by photoViewModel.photos.observeAsState(emptyList())
     var selectedPhoto by remember { mutableStateOf<Photo?>(null) }
     var editingName by remember { mutableStateOf<String?>(null)}
+    var search by remember { mutableStateOf("") }
+
+    val filteredPhotos = photos.filter {
+        it.name.contains(search, ignoreCase = true)
+    }
+
 
     Scaffold(
         topBar = {
@@ -74,21 +85,44 @@ fun BinderScreen(
                         .padding(16.dp),
                     textAlign = TextAlign.Center
                 )
-                Button(
-                    onClick = {
-                        navController.popBackStack()
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("Back")
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier
+                        .padding(8.dp)
+                ){
+                    Button(
+                        onClick = {
+                            navController.popBackStack()
+                        }
+                    ) {
+                        Text("Back")
+                    }
+
+                    TextField(
+                        value = search,
+                        onValueChange = { search = it },
+                        label = {Text("Search")},
+                        trailingIcon = {
+                            if(search.isNotEmpty()) {
+                                IconButton(onClick = { search = "" }) {
+                                    Icon(
+                                        imageVector = Icons.Default.Close,
+                                        contentDescription = "Clear"
+                                    )
+                                }
+                            }
+                        }
+                    )
+
                 }
+
                 LazyVerticalGrid(
                     columns = GridCells.Fixed(2),
                     modifier = Modifier
                         .fillMaxWidth()
                         .weight(1f)
                 ) {
-                    items(photos) { photo ->
+                    items(filteredPhotos) { photo ->
                         val isEditing = editingName == photo.name
                         var name by remember { mutableStateOf(photo.name)}
                         // Debug statement: log the photo's description
